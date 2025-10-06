e9813553c461b83ebe8a77f0d9e6a2684d17ace1
SimonKazantsev <misemen4@gmail.com>
2025-10-06 20:26:48 +0500

    начали появляться фотографии

--- app/src/main/java/com/example/project/MovieDetailScreen.kt
+++ app/src/main/java/com/example/project/MovieDetailScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.w3c.dom.Text
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
--- app/src/main/java/com/example/project/MovieListScreeen.kt
+++ app/src/main/java/com/example/project/MovieListScreeen.kt
@@ -1,13 +1,14 @@
 package com.example.project
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -16,8 +17,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 
 @Composable
@@ -34,13 +35,15 @@ fun MoviesListScreen(movies: List<Movie>, onMovieClick: (Int) -> Unit) {
                     .clickable { onMovieClick(movie.id) },
                 elevation = CardDefaults.cardElevation(4.dp)
             ) {
-                Column(modifier = Modifier.padding(16.dp)) {
+                Row(modifier = Modifier.padding(16.dp)) {
+                    AsyncImage(
+                        model = movie.photo,
+                        contentDescription = movie.title,
+                    )
                     Text(text = movie.title, style = MaterialTheme.typography.titleMedium)
                     Spacer(modifier = Modifier.height(4.dp))
                     Text(text = movie.description.take(60) + "...", style = MaterialTheme.typography.bodyMedium)
                     Text(text = "Год выпуска: " + movie.year, style = MaterialTheme.typography.bodyMedium)
-
-
 
                 }
             }
